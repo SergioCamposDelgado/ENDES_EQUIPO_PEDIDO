bb8845f29bcc8bcb4ce38a5678998581013e1443
josemiguelzerpagarcia <zerpa.garcia.jose.miguel@iescamas.es>
2025-04-01 12:00:56 +0200

    clase categoria terminada

--- codeJava/src/main/java/com/endes/resource/Categoria.java
+++ codeJava/src/main/java/com/endes/resource/Categoria.java
@@ -1,5 +1,37 @@
 package com.endes.resource;
 
 public class Categoria {
-
+		protected String nombre;
+		
+		//constructor vacio
+		public Categoria() {
+			
+		}
+		
+		/**
+		 * 
+		 * @param nombre: nombre de la categroia 
+		 */
+		public Categoria(String nombre) {
+			this.nombre = nombre;
+		}
+		
+		/**
+		 * 
+		 * @return: devuelve el nombre de la categoria
+		 */
+		public String getNombre() {
+			return nombre;
+		}
+		
+		/**
+		 * 
+		 * @param nombre: nombre de la categoria
+		 */
+		public void setNombre(String nombre) {
+			if (nombre == null || nombre.isBlank()) {
+				throw new IllegalArgumentException("El nombre no es válido");
+			}
+			this.nombre = nombre;
+		}
 }
